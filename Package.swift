a2f30fd79cbee16d541cbd74d65b59bbcfc548c5
mincheol-finda <133839139+mincheol-finda@users.noreply.github.com>
2023-05-17 13:18:31 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,13 +5,13 @@ import PackageDescription
 let package = Package(
   name: "RxExpect",
   platforms: [
-    .iOS(.v8), .macOS(.v10_10), .tvOS(.v9)
+    .iOS(.v9), .macOS(.v10_10), .tvOS(.v9)
   ],
   products: [
     .library(name: "RxExpect", targets: ["RxExpect"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
   ],
   targets: [
     .target(name: "RxExpect", dependencies: ["RxSwift", "RxTest"]),
